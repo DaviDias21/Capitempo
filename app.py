15af36edc88d41000b2c6a327061d6fe0282f30a
Davi Dias <51387816+DaviDias21@users.noreply.github.com>
2025-01-17 12:16:00 -0300

    Work in Progress

--- app.py
+++ app.py
@@ -8,16 +8,30 @@ app = Flask(__name__, template_folder="templates")
 if not os.path.exists("polls.csv"):
     structure = {
         "id": [],
-        "poll": [],
-        "option1": [],
-        "option2": [],
-        "option3": [],
-        "votes1": [],
-        "votes2": [],
-        "votes3": [],
-        "votesTotal": []
-    }
+        "area": [],
+        "location": [],
+        "c_pollResult": [],
+        "c_noOption": [],
+        "c_lowOption": [],
+        "c_avgOption": [],
+        "c_highOption": [],
+        "c_votesNo": [],
+        "c_votesLow": [],
+        "c_votesAvg": [],
+        "c_votesHigh": [],
+        "c_votesTotal": [],
 
+        "r_pollResult": [],
+        "r_noOption": [],
+        "r_lowOption": [],
+        "r_avgOption": [],
+        "r_highOption": [],
+        "r_votesNo": [],
+        "r_votesLow": [],
+        "r_votesAvg": [],
+        "r_votesHigh": [],
+        "r_votesTotal": []
+    }
     pd.DataFrame(structure).set_index("id").to_csv("polls.csv")
 
 polls_df = pd.read_csv("polls.csv").set_index("id")
@@ -36,28 +50,32 @@ def create_poll():
     if request.method == "GET":
         return render_template("new_poll.html")
     elif request.method == "POST":
-        poll = request.form['poll']
-        option1 = request.form['option1']
-        option2 = request.form['option2']
-        option3 = request.form['option3']
-
-        polls_df.loc[max(polls_df.index.values) + 1] = [poll,option1,option2,option3,0,0,0,0]
+        area = request.form['area']
+        location = request.form['location']
+        
+        polls_df.loc[max(polls_df.index.values) + 1] = [area,location,"Indeterminado","Céu limpo","Pouco nublado",
+                                                        "Nublado","Muito nublado",0,0,0,0,0,"Indeterminada","Sem chuva",
+                                                        "Chuva leve","Chuva","Chuva forte",0,0,0,0,0]
+        
         polls_df.to_csv("polls.csv")
         return redirect(url_for("index"))
 
-@app.route("/vote/<pollId>/<option>")
-def vote(pollId, option):
-    if request.cookies.get(f"vote_{pollId}_cookie") is None:
-        polls_df.at[int(pollId), "votes"+str(option)] += 1
-        polls_df.at[int(pollId), "votesTotal"] += 1
-        polls_df.to_csv("polls.csv")
-        response = make_response(redirect(url_for("polls", pollId=pollId)))
-        response.set_cookie(f"vote_{pollId}_cookie", str(option))
-        return response
-    
-    else:
-        return "Cannot vote more than once!"
+@app.route("/upvote/<pollId>/<option>")
+def upvote(pollId, pollPrefix ,option):
+    polls_df.at[int(pollId), str(pollPrefix)+"votes"+str(option)] += 1
+    polls_df.at[int(pollId), str(pollPrefix)+"votesTotal"] += 1
+    polls_df.to_csv("polls.csv")
+    response = make_response(redirect(url_for("polls", pollId=pollId)))
+    return response
 
+@app.route("/downvote/<pollId>/<option>")
+def downvote(pollId, option):
+    if(polls_df.at[int(pollId), "votes"+str(option)]) > 0:
+        polls_df.at[int(pollId), "votes"+str(option)] -= 1
+        polls_df.at[int(pollId), "votesTotal"] -= 1
+        polls_df.to_csv("polls.csv")
+    response = make_response(redirect(url_for("polls", pollId=pollId)))
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
